a82e71c21d139a00193ae0c8ffaa9987d236b8a5
Michael Reichenbach <michael@mirenoba.com>
2018-09-17 20:51:05 +0200

    use docker dns in api

--- client/libs/items/src/lib/services/items-api.service.ts
+++ client/libs/items/src/lib/services/items-api.service.ts
@@ -6,41 +6,41 @@ import { RCITEM_MOCK_DATA } from '../models/items.mock-data';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ItemsApiService {
 
-  private readonly BASE_URL = '/api/items';
+    private readonly BASE_URL = 'http://faldoria-api/api/items';
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  getAllItems(): Observable<RCItem[]> {
-    return this.http.get<any[]>(this.BASE_URL).pipe(
-      map(data => data.map(json => this.mapToItem(json)))
-    );
-  }
-
-  createItem(item: RCItem): Observable<RCItem> {
-    return this.http.post(this.BASE_URL, item.toApiFormat()).pipe(
-      map(data => this.mapToItem(data))
-    );
-  }
-
-  deleteItem(id: number): Observable<any> {
-    return this.http.delete(`${this.BASE_URL}/${id}`);
-  }
-
-  private mapToItem(data: any): RCItem {
-
-    switch (data.item_type) {
-      case 'EQUIPMENT':
-        return new RCEquipment({ ...data.equipment, ...data });
-      case 'ARMOR':
-        return new RCArmor({ ...data.equipment, ...data.equipment.armor, ...data });
-      case 'WEAPON':
-        return new RCWeapon({ ...data.equipment, ...data.equipment.weapon, ...data });
-      default:
-        return new RCItem(data);
+    getAllItems(): Observable<RCItem[]> {
+        return this.http.get<any[]>(this.BASE_URL).pipe(
+            map(data => data.map(json => this.mapToItem(json)))
+        );
     }
-  }
+
+    createItem(item: RCItem): Observable<RCItem> {
+        return this.http.post(this.BASE_URL, item.toApiFormat()).pipe(
+            map(data => this.mapToItem(data))
+        );
+    }
+
+    deleteItem(id: number): Observable<any> {
+        return this.http.delete(`${this.BASE_URL}/${id}`);
+    }
+
+    private mapToItem(data: any): RCItem {
+
+        switch (data.item_type) {
+            case 'EQUIPMENT':
+                return new RCEquipment({ ...data.equipment, ...data });
+            case 'ARMOR':
+                return new RCArmor({ ...data.equipment, ...data.equipment.armor, ...data });
+            case 'WEAPON':
+                return new RCWeapon({ ...data.equipment, ...data.equipment.weapon, ...data });
+            default:
+                return new RCItem(data);
+        }
+    }
 }

9668836e2557d2def29c36a00481c20e7768decc
Michael Reichenbach <michael@mirenoba.com>
2018-09-27 19:54:11 +0200

    fix(art): fix duplicate art objects in table

--- server/src/services/actionapi.service.ts
+++ server/src/services/actionapi.service.ts
@@ -3,6 +3,6 @@ import { ActionApi } from "models/actionapi.model";
 export class ActionApiService {
 
   public getAll() {
-    return ActionApi.all();
+    return ActionApi.findAll({ attributes: ['name', 'action_type', 'description', 'conf'], group: ['name', 'action_type'] });
   }
 }

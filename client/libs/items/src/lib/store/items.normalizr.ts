5ab9efb806f8ed67e10a8a7338dd8af6678aea1d
Michael Reichenbach <michael@mirenoba.com>
2018-10-04 18:54:35 +0200

    fix minecraft item display

--- client/libs/items/src/lib/store/items.normalizr.ts
+++ client/libs/items/src/lib/store/items.normalizr.ts
@@ -3,7 +3,6 @@ import { schema } from 'normalizr';
 export const itemCategorySchema = new schema.Entity('categories');
 export const minecraftItemSchema = new schema.Entity('minecraft_items');
 export const raidCraftItemSchema = new schema.Entity('items', {
-  minecraftItem: minecraftItemSchema,
   categories: [itemCategorySchema]
 });
 
